2fb23bc3607d0c327411aca75f1b765728168927
Burhan Akkus <burgcanmail@gmail.com>
2025-05-18 23:05:41 +0200

    dataFrame with Indexing

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import pandas as pd
+
+print(pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]}))
+
+print(pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']}))
+
+print(pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'],
+              'Sue': ['Pretty good.', 'Bland.']},
+             index=['Product A', 'Product B']))

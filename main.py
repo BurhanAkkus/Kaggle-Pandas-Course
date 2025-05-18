ce2ae6b69d3ed41ab6b841cedffc04fd70756bfb
Burhan Akkus <burgcanmail@gmail.com>
2025-05-18 23:06:56 +0200

    comments

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
+# Generic Construction
 print(pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]}))
 
+
+# Flexible Datatypes
 print(pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']}))
 
+
+# Custom Indexing
 print(pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'],
               'Sue': ['Pretty good.', 'Bland.']},
              index=['Product A', 'Product B']))
+
+# Series is a list
+print(pd.Series([1, 2, 3, 4, 5]))
